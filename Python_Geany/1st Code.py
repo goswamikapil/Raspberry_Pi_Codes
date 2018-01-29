f19f8a6ed518deb27fe91d62a96ff4786d1367d8
kapil goswami <gkapil45@gmail.com>
2018-01-29 11:50:46 +0530

    Update 1st Code.py

--- Python_Geany/1st Code.py
+++ Python_Geany/1st Code.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 count = 0
 
-while(count < 100):
+while(count < 10):
 	 print  "hello world"
 	 count = count + 1
 
 print "good bye"
+
+
+
+"""
+OUTPUT:-
+
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+good bye
+
+"""
